1507669399e11b4858422dd5fdd028ead9f1c9ff
R E Broadley <rebroad@users.noreply.github.com>
2026-08-04 10:59:59 +0100

    fix: support ARMv7 release builds

--- codex-rs/linux-sandbox/src/landlock.rs
+++ codex-rs/linux-sandbox/src/landlock.rs
@@ -180,8 +180,8 @@ fn install_network_seccomp_filter_on_current_thread(
     mode: NetworkSeccompMode,
     managed_network: Option<&ManagedNetworkSandboxContext>,
 ) -> std::result::Result<(), SandboxErr> {
-    fn deny_syscall(rules: &mut BTreeMap<i64, Vec<SeccompRule>>, nr: i64) {
-        rules.insert(nr, vec![]); // empty rule vec = unconditional match
+    fn deny_syscall(rules: &mut BTreeMap<i64, Vec<SeccompRule>>, nr: impl Into<i64>) {
+        rules.insert(nr.into(), vec![]); // empty rule vec = unconditional match
     }
 
     // Build rule map.
@@ -227,8 +227,8 @@ fn install_network_seccomp_filter_on_current_thread(
                 libc::AF_UNIX as u64,
             )?])?;
 
-            rules.insert(libc::SYS_socket, vec![unix_only_rule.clone()]);
-            rules.insert(libc::SYS_socketpair, vec![unix_only_rule]);
+            rules.insert(libc::SYS_socket.into(), vec![unix_only_rule.clone()]);
+            rules.insert(libc::SYS_socketpair.into(), vec![unix_only_rule]);
         }
         NetworkSeccompMode::ProxyRouted => {
             // In proxy-routed mode we allow IP sockets in the isolated
@@ -264,8 +264,8 @@ fn install_network_seccomp_filter_on_current_thread(
                 SeccompCmpOp::Ne,
                 libc::AF_UNIX as u64,
             )?])?;
-            rules.insert(libc::SYS_socket, vec![deny_non_ip_socket]);
-            rules.insert(libc::SYS_socketpair, vec![deny_non_unix_socketpair]);
+            rules.insert(libc::SYS_socket.into(), vec![deny_non_ip_socket]);
+            rules.insert(libc::SYS_socketpair.into(), vec![deny_non_unix_socketpair]);
         }
         NetworkSeccompMode::VmSocketRestricted => {
             let deny_vsock = SeccompRule::new(vec![SeccompCondition::new(
