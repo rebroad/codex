5989136ee5bf5e8e2227a8affe4c00186fb9e485
R E Broadley <rebroad@users.noreply.github.com>
2026-08-11 21:07:18 +0100

    fix(armv7): centralize cross-build artifacts

--- codex-rs/linux-sandbox/src/landlock.rs
+++ codex-rs/linux-sandbox/src/landlock.rs
@@ -224,8 +224,8 @@ fn install_network_seccomp_filter_on_current_thread(
                 libc::AF_UNIX as u64,
             )?])?;
 
-            rules.insert(libc::SYS_socket, vec![unix_only_rule.clone()]);
-            rules.insert(libc::SYS_socketpair, vec![unix_only_rule]);
+            rules.insert(libc::SYS_socket.into(), vec![unix_only_rule.clone()]);
+            rules.insert(libc::SYS_socketpair.into(), vec![unix_only_rule]);
         }
         NetworkSeccompMode::IsolatedLocalIpc => {
             // Bubblewrap has already unshared the network namespace before
@@ -253,8 +253,8 @@ fn install_network_seccomp_filter_on_current_thread(
                 libc::AF_UNIX as u64,
             )?])?;
 
-            rules.insert(libc::SYS_socket, vec![deny_non_ip_socket]);
-            rules.insert(libc::SYS_socketpair, vec![unix_only_rule]);
+            rules.insert(libc::SYS_socket.into(), vec![deny_non_ip_socket]);
+            rules.insert(libc::SYS_socketpair.into(), vec![unix_only_rule]);
         }
         NetworkSeccompMode::ProxyRouted => {
             // In proxy-routed mode we allow IP sockets in the isolated
@@ -283,8 +283,8 @@ fn install_network_seccomp_filter_on_current_thread(
                 SeccompCmpOp::Ne,
                 libc::AF_UNIX as u64,
             )?])?;
-            rules.insert(libc::SYS_socket, vec![deny_non_ip_socket]);
-            rules.insert(libc::SYS_socketpair, vec![deny_non_unix_socketpair]);
+            rules.insert(libc::SYS_socket.into(), vec![deny_non_ip_socket]);
+            rules.insert(libc::SYS_socketpair.into(), vec![deny_non_unix_socketpair]);
         }
     }
 
