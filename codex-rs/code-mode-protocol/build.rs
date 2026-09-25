7d421f1909c6f0c881e7b060f940906590cda5b4
R E Broadley <rebroad@users.noreply.github.com>
2026-09-25 07:27:52 +0100

    build(android): add ARMv7 cross-build workflow

--- codex-rs/code-mode-protocol/build.rs
+++ codex-rs/code-mode-protocol/build.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -5,7 +6,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=src/grpc");
 
     let mut config = tonic_prost_build::Config::new();
-    config.protoc_executable(protoc_bin_vendored::protoc_bin_path()?);
+    let protoc = env::var_os("PROTOC")
+        .map(PathBuf::from)
+        .or_else(|| {
+            env::var_os("PATH")
+                .into_iter()
+                .flat_map(|path| env::split_paths(&path).collect::<Vec<_>>())
+                .map(|directory| directory.join("protoc"))
+                .find(|path| path.is_file())
+        })
+        .map(Ok)
+        .unwrap_or_else(protoc_bin_vendored::protoc_bin_path)?;
+    config.protoc_executable(protoc);
     let proto_files = glob::glob("src/grpc/*.proto")?.collect::<Result<Vec<_>, _>>()?;
 
     tonic_prost_build::configure()
